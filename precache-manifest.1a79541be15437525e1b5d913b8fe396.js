71bcac37d5fd8e79740c7b75790ecdba044393ff
andypalmera <andreapalmerita@hotmail.com>
2020-07-17 14:16:13 -0300

    Updates

---
+++ precache-manifest.1a79541be15437525e1b5d913b8fe396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8ee899b8544e5b779001471a4bbe31",
+    "url": "/reloj-react/index.html"
+  },
+  {
+    "revision": "a6018338348d58640b0b",
+    "url": "/reloj-react/static/css/main.33bf8612.chunk.css"
+  },
+  {
+    "revision": "2af51e62008086084370",
+    "url": "/reloj-react/static/js/2.ac09f172.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reloj-react/static/js/2.ac09f172.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6018338348d58640b0b",
+    "url": "/reloj-react/static/js/main.5e2205c3.chunk.js"
+  },
+  {
+    "revision": "8e9ae01f3c203809080e",
+    "url": "/reloj-react/static/js/runtime-main.025cb3e0.js"
+  }
+]);
